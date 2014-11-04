2b60198ccf110457cd06dade89ccb5e804267a49
Mr1024 <publiczhongemail@163.com>
2014-11-04 18:56:39 +0800

    modify

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 var http = require('http');
+var path = require('path');
+var url = require('url');
 var portscanner = require('portsacnner');
 http.createServer(function(req, res) {
-
+    var reqURL = req.url;
+    var pathname = url.parse(reqURL).pathname;
+    path.exists(pathname, function(exists) {
+        if (!exists) {
+            req.writeHead('404', {
+                'Content-Type': 'text/plain'
+            });
+            req.write();
+        }
+    });
 });
