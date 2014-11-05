5ea60ad37dd259560d768fde71eeb3de887363b0
Mr1024 <publiczhongemail@163.com>
2014-11-05 17:16:30 +0800

    modify the index.js

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ var http = require('http');
 var path = require('path');
 var url = require('url');
 var fs = require('fs');
+var zlib = require('zlib');
 var portscanner = require('portscanner');
-var mime = requrie('lib/mime.js');
+var mime = require('./lib/mime');
+var config = require('./lib/config');
 http.createServer(function(req, res) {
     var reqURL = req.url;
+    var reqHeader = req.headers;
     var pathname = url.parse(reqURL).pathname;
     var filePath = process.cwd() + pathname;
     var ext = path.extname(pathname);
@@ -19,20 +22,99 @@ http.createServer(function(req, res) {
             res.write('This request  URL ' + pathname + ' was not found');
             res.end();
         } else {
+            fs.stat(filePath, function(err, stat) {
+                if (!err) {
+                    var raw = fs.createReadStream(filePath);
+                    var acceptEncoding = reqHeader['accept-encoding'] || "";
+                    var matched = ext.match(config.Compress.match);
+                    var lastModified = stat.mtime.toUTCString();
+                    res.setHeader('Last-Modified', lastModified);
+                    if (reqHeader['ifModifiedSince'] && lastModified == reqHeader['ifModifiedSince']) {
+                        res.writeHead(304, "Not Modified");
+                    } else {
+                        if (ext.match(config.Expires.fileMatch)) {
+                            var expires = new Date();
+                            expires.setTime(expires.getTime() + config.Expires.maxAge * 1000);
+                            res.setHeader("Expires", expires.toUTCString());
+                            res.setHeader('Cache-Control', "max-age=" + config.Expires.maxAge);
+                        }
+                        if (matched && acceptEncoding.match(/\bgzip\b/)) {
+                            res.writeHead(200, "Ok", {
+                                'Content-Encoding': 'gzip'
+                            });
+                            raw.pipe(zlib.createGzip()).pipe(res);
+                        } else if (matched && acceptEncoding.match(/\bdeflate\b/)) {
+                            res.writeHead(200, "Ok", {
+                                'Content-Encoding': 'deflate'
+                            });
+                            raw.pipe(zlib.createDeflate()).pipe(res);
+                        } else {
+                            res.writeHead(200, {
+                                'Content-Type': contentType
+                            });
+                            raw.pipe(res);
+                        }
+                        raw.on('error',function() {
+                            res.writeHead(500, {
+                                'Content-Type': 'text/plain'
+                            });
+                            res.end(err);
+                        });
+                    }
+                }
+
+            });
+            /*var raw = fs.createReadStream(filePath);
+            var acceptEncoding = reqHeader['accept-encoding'] || "";
+            var matched = ext.match(config.Compress.match);
+            if (matched && acceptEncoding.match(/\bgzip\b/)) {
+                res.writeHead(200, "Ok", {
+                    'Content-Encoding': 'gzip'
+                });
+                raw.pipe(zlib.createGzip()).pipe(res);
+            } else if (matched && acceptEncoding.match(/\bdeflate\b/)) {
+                res.writeHead(200, "Ok", {
+                    'Content-Encoding': 'deflate'
+                });
+                raw.pipe(zlib.createDeflate()).pipe(res);
+            } else {
+                res.writeHead(200, "Ok");
+                raw.pipe(res);
+            }
+            raw.on('error', function()function() {
+                res.writeHead(500, {
+                    'Content-Type': 'text/plain'
+                });
+                res.end(err);
+            });
             fs.readFile(filePath, 'binary', function(err, file) {
                 if (err) {
                     res.writeHead(500, {
                         'Content-Type': 'text/plain'
                     });
-                    res.write(err);
+                    res.end(err);
                 } else {
-                    res.writeHead(200, {
-                        'Content-Type': contentType
+                    fs.stat(filePath, function(err, stat) {
+                        var lastModified = stat.mtime.toUTCString();
+                        res.setHeader('Last-Modified', lastModified);
+                        if (reqHeader['ifModifiedSince'] && lastModified == reqHeader['ifModifiedSince']) {
+                            res.writeHead(304, "Not Modified");
+                        } else {
+                            if (ext.match(config.Expires.fileMatch)) {
+                                var expires = new Date();
+                                expires.setTime(expires.getTime() + config.Expires.maxAge * 1000);
+                                res.setHeader("Expires", expires.toUTCString());
+                                res.setHeader('Cache-Control', "max-age=" + config.Expires.maxAge);
+                            }
+                            res.writeHead(200, {
+                                'Content-Type': contentType
+                            });
+                            res.write(file, "binary");
+                        }
+                        res.end();
                     });
-                    res.write(file, "binary");
-                    res.end();
                 }
-            });
+            });*/
         }
     });
 }).listen(3000);
